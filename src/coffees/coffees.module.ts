6234e5047b8d922b7b0eddd8b806338e94b71b82
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-14 22:54:52 +0330

    add coffee table to database

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoffeesResolver } from './coffees.resolver';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   providers: [CoffeesResolver, CoffeesService],
 })
 export class CoffeesModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,10 +1,19 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType({ description: 'coffees model' })
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'unique identifier' })
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column({ type: 'json' })
   flavors: string[];
 }
