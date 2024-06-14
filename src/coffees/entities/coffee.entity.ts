cb8000e61170828558ec2c3a7892eede3936f805
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-14 17:42:38 +0330

    add findone method in coffee resolver

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,8 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
-@ObjectType()
+@ObjectType({ description: 'coffees model' })
 export class Coffee {
+  @Field(() => ID, { description: 'unique identifier' })
   id: number;
   name: string;
   brand: string;
